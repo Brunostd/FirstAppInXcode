2dd42bd542823acdf791c706ef41bdbd6a705e97
brunobrito <batioe.std@gmail.com>
2022-03-26 20:12:26 -0300

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Bruno Brito on 26/03/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
